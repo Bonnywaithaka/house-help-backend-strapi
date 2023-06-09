12255508488b1bd6d7fb57048d20f8850aaeb4f5
bthairu <bthairu@safaricom.co.ke>
2023-06-09 09:19:16 +0300

    new task

---
+++ src/api/house-help/controllers/house-help.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * house-help controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::house-help.house-help');
---
+++ src/api/house-help/services/house-help.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * house-help service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::house-help.house-help');
